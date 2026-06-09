3d548150d64c5d63fceb63afd10157c2ad606e95
remieliakakpo@gmail.com <138904793+Remak-tech@users.noreply.github.com>
2026-06-09 18:14:55 +0000

    fix: hardcode supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL  = process.env.REACT_APP_SUPABASE_URL!;
-const SUPABASE_ANON = process.env.REACT_APP_SUPABASE_ANON_KEY!;
+const SUPABASE_URL  = 'https://xtalrmoacijdwioazfps.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0YWxybW9hY2lqZHdpb2F6ZnBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MzI3MTcsImV4cCI6MjA5MTQwODcxN30.rVTdM7KNgWDC3r8MQ7_TVOf-xZd4j_XeFTJXeLXBHcA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON, {
   realtime: { params: { eventsPerSecond: 10 } },
